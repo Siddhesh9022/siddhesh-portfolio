9aeccc45177915b99a94392b6edfe0839daf2cb5
Siddhesh-Patil-05 <siddheshpatil784@gmail.com>
2026-03-02 12:45:42 +0530

    Folder Structure

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-3xl, text-cyan-500'>Siddhesh Patil - Portfolio</h1>
+    </div>
+  )
+  
+
+
+}
+
+export default App
